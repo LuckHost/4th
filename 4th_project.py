44440e9aab9d2c1f6ea55db2e90295da5ac1d5f5
LuckHost <sh76484@gmail.com>
2023-11-29 22:16:30 +0700

    App class and image creator has been added

--- 4th_project.py
+++ 4th_project.py
@@ -2,20 +2,28 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 
+class App():
+    def __init__(self, master):
+        frm = ttk.Frame(master, padding =10)
+        frm.pack()
+        
+        self.canvas = Canvas(width = 300, height = 300, bg = 'blue')
+        self.but = Button(master, text="Click me!", command=self.get_image)
+        self.canvas.create_window(10, 10, anchor="nw", window=self.but)
+        self.canvas.pack()
+        
+    def get_image(self):
+        self.background_image = ImageTk.PhotoImage(file="bg.jpg")
+        self.canvas.create_image(0, 0, image = self.background_image, anchor = "nw")
+        
+
 def main():
     """main function"""
     root = Tk()
     root.geometry("300x300")
-    
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-    
-    background_image = ImageTk.PhotoImage(Image.open("bg.jpg"))
-    background_label = ttk.Label(root, image=background_image)
-    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+    App(root)
     root.mainloop()
+    #frm = ttk.Frame(root, padding=10)
     
 if __name__ == "__main__":
     main()
